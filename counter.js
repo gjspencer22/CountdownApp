2b12823d5f3590faa644fdcdd90a4d097848a5ca
gjspencer22 <gjspencer22@gmail.com>
2021-11-05 18:32:58 -0600

    changed to christmas

--- counter.js
+++ counter.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Sept 15, 2021 05:32:00").getTime();
+var countDownDate = new Date("Dec 25, 2021 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -23,6 +23,6 @@ var x = setInterval(function() {
   // If the count down is over, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("number").innerHTML = "EXPIRED";
+    document.getElementById("number").innerHTML = "Merry Christmas";
   }
 }, 1000);
